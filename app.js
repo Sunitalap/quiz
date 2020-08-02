878920598c9beb640c08b5e1e3bc51108f808f64
ohtsuki <ohtsuki@cubesystem.co.jp>
2020-08-02 10:18:22 +0900

    local起動対応

---
+++ app.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (request, response) {
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  target = './index.html';
+
+  fs.readFile(target, 'utf-8', function (err, data) {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(data);
+    response.end();
+  })
+}).listen(8080);
+
+// 起動ログ
+console.log('Server running at http://127.0.0.1:8080/');
