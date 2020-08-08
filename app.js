64b17939ff08a5853e4ae2f5bc947e4fefaf6d25
ohtsuki <ohtsuki@cubesystem.co.jp>
2020-08-08 11:53:30 +0900

    回答データをlocalstrageに保存

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+var express = require('express');
 
 http.createServer(function (request, response) {
   response.writeHead(200, { 'Content-Type': 'text/plain' });
